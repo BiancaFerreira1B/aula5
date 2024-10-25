95512db1b20b89642d338f115b0997d4aa4bb681
bianca2B <143563325+BiancaFerreira1B@users.noreply.github.com>
2024-10-25 19:40:48 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Zootecnia',
+    'É igual a veterinária?',
+    'Não. O zootecnista previne e o veterinário trata.'
+)
+
+criaCartao(
+    'Agronomia',
+    'Lida só em campo?',
+    'Não. Tem aulas práticas e teóricas.'
+)
+
+criaCartao(
+    'Zootecnia',
+    'Qual a finalidade da zootecnia?',
+    'Promover e aplicar medidas de fomento à produção animal.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz OI em inglês?',
+    'Oi em ingles é HI (RAI)'
+) 
